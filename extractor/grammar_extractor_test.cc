bfa6023542e5689baed8c61fece48d1d2a2ab57d
Paul Baltescu <pauldb89@gmail.com>
2014-02-13 22:39:22 +0000

    Grammar extraction for lattices.

--- extractor/grammar_extractor_test.cc
+++ extractor/grammar_extractor_test.cc
@@ -6,9 +6,15 @@
 
 #include "grammar.h"
 #include "grammar_extractor.h"
-#include "mocks/mock_rule_factory.h"
+#include "lattice.h"
+#include "mocks/mock_fast_intersector.h"
+#include "mocks/mock_matchings_finder.h"
+#include "mocks/mock_rule_extractor.h"
+#include "mocks/mock_sampler.h"
+#include "mocks/mock_scorer.h"
 #include "mocks/mock_vocabulary.h"
-#include "rule.h"
+#include "phrase_builder.h"
+#include "phrase_location.h"
 
 using namespace std;
 using namespace ::testing;
@@ -16,38 +22,99 @@ using namespace ::testing;
 namespace extractor {
 namespace {
 
-TEST(GrammarExtractorTest, TestAnnotatingWords) {
-  shared_ptr<MockVocabulary> vocabulary = make_shared<MockVocabulary>();
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("<s>"))
-      .WillRepeatedly(Return(0));
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("Anna"))
-      .WillRepeatedly(Return(1));
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("has"))
-      .WillRepeatedly(Return(2));
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("many"))
-      .WillRepeatedly(Return(3));
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("apples"))
-      .WillRepeatedly(Return(4));
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("."))
-      .WillRepeatedly(Return(5));
-  EXPECT_CALL(*vocabulary, GetTerminalIndex("</s>"))
-      .WillRepeatedly(Return(6));
+class GrammarExtractorTest : public Test {
+ protected:
+  virtual void SetUp() {
+    finder = make_shared<MockMatchingsFinder>();
+    fast_intersector = make_shared<MockFastIntersector>();
 
-  shared_ptr<MockHieroCachingRuleFactory> factory =
-      make_shared<MockHieroCachingRuleFactory>();
-  vector<int> word_ids = {0, 1, 2, 3, 3, 4, 5, 6};
-  vector<Rule> rules;
+    vocabulary = make_shared<MockVocabulary>();
+    EXPECT_CALL(*vocabulary, GetTerminalValue(2)).WillRepeatedly(Return("a"));
+    EXPECT_CALL(*vocabulary, GetTerminalValue(3)).WillRepeatedly(Return("b"));
+    EXPECT_CALL(*vocabulary, GetTerminalValue(4)).WillRepeatedly(Return("c"));
+    EXPECT_CALL(*vocabulary, GetTerminalValue(100))
+        .WillRepeatedly(Return("*EPS*"));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("a")).WillRepeatedly(Return(2));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("b")).WillRepeatedly(Return(3));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("c")).WillRepeatedly(Return(4));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("*EPS*"))
+        .WillRepeatedly(Return(100));
+
+    phrase_builder = make_shared<PhraseBuilder>(vocabulary);
+
+    scorer = make_shared<MockScorer>();
+    feature_names = {"f1"};
+    EXPECT_CALL(*scorer, GetFeatureNames())
+        .WillRepeatedly(Return(feature_names));
+
+    sampler = make_shared<MockSampler>();
+    EXPECT_CALL(*sampler, Sample(_, _))
+        .WillRepeatedly(Return(PhraseLocation(0, 1)));
+
+    Phrase phrase;
+    vector<double> scores = {0.5};
+    vector<pair<int, int>> phrase_alignment = {make_pair(0, 0)};
+    vector<Rule> rules = {Rule(phrase, phrase, scores, phrase_alignment)};
+    rule_extractor = make_shared<MockRuleExtractor>();
+    EXPECT_CALL(*rule_extractor, ExtractRules(_, _))
+        .WillRepeatedly(Return(rules));
+
+    grammar_extractor = make_shared<GrammarExtractor>(
+        finder, fast_intersector, phrase_builder, rule_extractor, vocabulary,
+        sampler, scorer, 1, 10, 2, 3, 5);
+  }
+
+  void CheckInput(
+      const string& input, bool sentence,
+      int contiguous_rules, int discontiguous_rules) {
+    EXPECT_CALL(*finder, Find(_, _, _))
+        .Times(contiguous_rules)
+        .WillRepeatedly(Return(PhraseLocation(0, 1)));
+    EXPECT_CALL(*fast_intersector, Intersect(_, _, _))
+        .Times(discontiguous_rules)
+        .WillRepeatedly(Return(PhraseLocation(0, 1)));
+
+    Lattice lattice(input, vocabulary, sentence);
+    unordered_set<int> blacklisted_sentence_ids;
+    Grammar grammar = grammar_extractor->GetGrammar(
+        lattice, blacklisted_sentence_ids);
+
+    EXPECT_EQ(feature_names, grammar.GetFeatureNames());
+    EXPECT_EQ(contiguous_rules + discontiguous_rules,
+              grammar.GetRules().size());
+  }
+
   vector<string> feature_names;
-  Grammar grammar(rules, feature_names);
-  unordered_set<int> blacklisted_sentence_ids;
-  shared_ptr<DataArray> source_data_array;
-  EXPECT_CALL(*factory, GetGrammar(word_ids, blacklisted_sentence_ids))
-      .WillOnce(Return(grammar));
+  shared_ptr<MockMatchingsFinder> finder;
+  shared_ptr<MockFastIntersector> fast_intersector;
+  shared_ptr<MockVocabulary> vocabulary;
+  shared_ptr<PhraseBuilder> phrase_builder;
+  shared_ptr<MockScorer> scorer;
+  shared_ptr<MockSampler> sampler;
+  shared_ptr<MockRuleExtractor> rule_extractor;
+  shared_ptr<GrammarExtractor> grammar_extractor;
+};
 
-  GrammarExtractor extractor(vocabulary, factory);
-  string sentence = "Anna has many many apples .";
+TEST_F(GrammarExtractorTest, TestGetGrammarDifferentWords) {
+  CheckInput("a b c", true, 6, 1);
+}
 
-  extractor.GetGrammar(sentence, blacklisted_sentence_ids);
+TEST_F(GrammarExtractorTest, TestGetGrammarRepeatingWords) {
+  CheckInput("a b c a b", true, 12, 16);
+}
+
+TEST_F(GrammarExtractorTest, TestLattice) {
+  string input = "((('a', 0, 1),('b', 0, 2),),(('c', 0, 3),('b', 0, 2),),"
+                 "(('a', 0, 1),),(('a', 0, 1),('a', 0, 2),),(('a', 0, 2),),"
+                 "(('c', 0, 1),),)";
+  CheckInput(input, false, 18, 12);
+}
+
+TEST_F(GrammarExtractorTest, TestEpsilonTransitions) {
+  string input = "((('a', 0, 1),('*EPS*', 0, 2),),(('*EPS*', 0, 3),('b', 0, 2),),"
+                 "(('a', 0, 1),),(('b', 0, 1),('c', 0, 2),),(('c', 0, 2),),"
+                 "(('a', 0, 1),),)";
+  CheckInput(input, false, 15, 9);
 }
 
 } // namespace
---
+++ extractor/lattice_test.cc
@@ -0,0 +1,208 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+
+#include "lattice.h"
+#include "mocks/mock_vocabulary.h"
+
+using namespace std;
+using namespace ::testing;
+
+namespace extractor {
+namespace {
+
+class LatticeTest : public Test {
+ protected:
+  virtual void SetUp() {
+    vocabulary = make_shared<MockVocabulary>();
+    for (int i = 0; i < 10; ++i) {
+      string word(1, i + 'a');
+      EXPECT_CALL(*vocabulary, GetTerminalValue(i + 2))
+          .WillRepeatedly(Return(word));
+      EXPECT_CALL(*vocabulary, GetTerminalIndex(word))
+          .WillRepeatedly(Return(i + 2));
+    }
+
+    EXPECT_CALL(*vocabulary, GetTerminalValue(100))
+        .WillRepeatedly(Return("*EPS*"));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("*EPS*"))
+        .WillRepeatedly(Return(100));
+    EXPECT_CALL(*vocabulary, GetTerminalValue(200))
+        .WillRepeatedly(Return("n\'t"));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("n\'t"))
+        .WillRepeatedly(Return(200));
+    EXPECT_CALL(*vocabulary, GetTerminalValue(201))
+        .WillRepeatedly(Return("a\\b"));
+    EXPECT_CALL(*vocabulary, GetTerminalIndex("a\\b"))
+        .WillRepeatedly(Return(201));
+  }
+
+  shared_ptr<MockVocabulary> vocabulary;
+};
+
+/*
+
+TEST_F(LatticeTest, TestSentence) {
+  string input = "a b c";
+  Lattice lattice(input, vocabulary, true);
+
+  EXPECT_EQ(4, lattice.GetSize());
+
+  vector<pair<int, int>> expected_transitions;
+  for (int i = 0; i < 3; ++i) {
+    expected_transitions = {{i + 1, i + 2}};
+    EXPECT_EQ(expected_transitions, lattice.GetTransitions(i));
+  }
+  expected_transitions = {};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(3));
+
+  vector<pair<int, int>> expected_extensions = {{1, 1}, {2, 2}, {3, 3}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 10));
+  expected_extensions = {{1, 1}, {2, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 2));
+  expected_extensions = {{2, 2}, {3, 3}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 2, 3));
+  expected_extensions = {{2, 1}, {3, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 3));
+  expected_extensions = {{2, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 1));
+  expected_extensions = {{3, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(2, 1, 5));
+  expected_extensions = {};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(2, 2, 3));
+}
+
+TEST_F(LatticeTest, TestDAG) {
+  string input = "((('a', 0, 1),),(('b', 0, 2),('c', -0.2, 1),('d', -1.5, 1),),"
+                 "(('e', 0, 1),),(('f', 0, 2),('g', -2.0, 1),),"
+                 "(('h', -1.0, 1),),)";
+  Lattice lattice(input, vocabulary, false);
+
+  EXPECT_EQ(6, lattice.GetSize());
+
+  vector<pair<int, int>> expected_transitions = {{1, 2}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(0));
+  expected_transitions = {{3, 3}, {2, 4}, {2, 5}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(1));
+  expected_transitions = {{3, 6}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(2));
+  expected_transitions = {{5, 7}, {4, 8}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(3));
+  expected_transitions = {{5, 9}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(4));
+  expected_transitions = {};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(5));
+
+  vector<pair<int, int>> expected_extensions =
+      {{1, 1}, {2, 2}, {3, 2}, {4, 3}, {5, 3}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 10));
+  expected_extensions = {{1, 1}, {2, 2}, {3, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 2));
+  expected_extensions = {{4, 3}, {5, 3}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 3, 5));
+  expected_extensions = {{2, 1}, {3, 1}, {4, 2}, {5, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 2));
+  expected_extensions = {{2, 1}, {3, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 1));
+  expected_extensions = {{4, 2}, {5, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 2, 2));
+  expected_extensions = {{3, 1}, {4, 2}, {5, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(2, 1, 2));
+  expected_extensions = {{4, 1}, {5, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(3, 1, 1));
+  expected_extensions = {{5, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(4, 1, 1));
+  expected_extensions = {};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(5, 1, 10));
+}
+
+TEST_F(LatticeTest, TestEpsilonTransitions) {
+  string input = "((('a', 0, 1),('*EPS*', 0, 2),),(('*EPS*', 0, 3),('b', 0, 2),),"
+                 "(('c', 0, 1),),(('d', 0, 1),('e', 0, 2),),(('f', 0, 2),),"
+                 "(('g', 0, 1),),)";
+  Lattice lattice(input, vocabulary, false);
+
+  EXPECT_EQ(7, lattice.GetSize());
+
+  vector<pair<int, int>> expected_transitions = {{4, 2}, {3, 4}, {1, 2}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(0));
+  expected_transitions = {{6, 7}, {3, 3}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(1));
+  expected_transitions = {{3, 4}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(2));
+  expected_transitions = {{5, 6}, {4, 5}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(3));
+  expected_transitions = {{6, 7}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(4));
+  expected_transitions = {{6, 8}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(5));
+  expected_transitions = {};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(6));
+
+  vector<pair<int, int>> expected_extensions =
+      {{1, 1}, {3, 1}, {4, 1}, {5, 2}, {6, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 10));
+  expected_extensions = {{1, 1}, {3, 1}, {4, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 1));
+  expected_extensions = {{5, 2}, {6, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 2, 2));
+  expected_extensions = {{3, 1}, {4, 2}, {5, 2}, {6, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 2));
+  expected_extensions = {{3, 1}, {6, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 1));
+  expected_extensions = {{4, 2}, {5, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 2, 2));
+  expected_extensions = {{3, 1}, {4, 2}, {5, 2}, {6, 3}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(2, 1, 3));
+  expected_extensions = {{4, 1}, {5, 1}, {6, 2}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(3, 1, 3));
+  expected_extensions = {{6, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(4, 1, 3));
+  expected_extensions = {{6, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(5, 1, 3));
+  expected_extensions = {};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(6, 1, 3));
+}
+
+TEST_F(LatticeTest, TestChainedEpsilonTransitions) {
+  string input = "((('*EPS*', 0, 1),),(('*EPS*', 0, 1),),(('a', 0, 1),),"
+                 "(('*EPS*', 0, 1),),(('*EPS*', 0, 1),),)";
+  Lattice lattice(input, vocabulary, false);
+
+  EXPECT_EQ(6, lattice.GetSize());
+
+  vector<pair<int, int>> expected_transitions = {{5, 2}, {4, 2}, {3, 2}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(0));
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(1));
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(2));
+  expected_transitions = {};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(3));
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(4));
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(5));
+
+  vector<pair<int, int>> expected_extensions = {{3, 1}, {4, 1}, {5, 1}};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(0, 1, 10));
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(1, 1, 10));
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(2, 1, 10));
+  expected_extensions = {};
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(3, 1, 10));
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(4, 1, 10));
+  EXPECT_EQ(expected_extensions, lattice.GetExtensions(5, 1, 10));
+}
+*/
+
+TEST_F(LatticeTest, TestEscapedSymbols) {
+  string input = "((('n\\\'t', 0, 1),),(('a\\\\b', 0, 1),),)";
+  Lattice lattice(input, vocabulary, false);
+
+  EXPECT_EQ(3, lattice.GetSize());
+  vector<pair<int, int>> expected_transitions = {{1, 200}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(0));
+  expected_transitions = {{2, 201}};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(1));
+  expected_transitions = {};
+  EXPECT_EQ(expected_transitions, lattice.GetTransitions(2));
+}
+
+} // namespace
+} // namespace extractor
